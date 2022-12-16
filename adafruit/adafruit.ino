6038ec6567b0e36b356645240e56a4b0ea7ecaa5
easton <eastonking1@gmail.com>
2022-12-16 13:45:00 +0800

    new changes to file

--- adafruit/adafruit.ino
+++ adafruit/adafruit.ino
@@ -13,7 +13,7 @@
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    "ph11"
-#define AIO_KEY         "aio_TWyU43RFExRtjLgijm2hphcq54fF"
+#define AIO_KEY         "aio_MxRt10yVNJzlgpUVSIeAEhx5JUJT"
 
 #define MQ135  35
 #define MQ7    34
@@ -49,10 +49,13 @@ Adafruit_MQTT_Publish gases = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/
 
 
 // Input to live feed on Adafruit
-//AdafruitIO_Feed *MQ135digital = io.feed("mq135");
-//AdafruitIO_Feed *MQ7digital = io.feed("mq7");
-AdafruitIO_Feed *PM25digital = io.feed("pm2-dot-5");
-AdafruitIO_Feed *PM10digital = io.feed("pm10");
+AdafruitIO_Feed *MQ135digital = io.feed("mq-air-quality-1.mq135-1");
+AdafruitIO_Feed *MQ7digital = io.feed("mq-air-quality-1.mq7-1");
+AdafruitIO_Feed *PM25digital = io.feed("mq-air-quality-1.pm25-1"); // pm25 = pm2.5
+AdafruitIO_Feed *PM10digital = io.feed("mq-air-quality-1.pm10-1");
+
+//AdafruitIO_Feed *wifiOn = io.feed("wifiActive");
+//wifiOn->save(0);
 
 void setup() {
   my_sds.begin(&port);
@@ -102,6 +105,7 @@ void MQTT_connect() {
 
   // Stop if already connected.
   if (mqtt.connected()) {
+//    wifiOn->save(1);
     return;
   }
 
@@ -113,6 +117,8 @@ void MQTT_connect() {
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds…");
     mqtt.disconnect();
+
+//    wifiOn->save(0);
 
     delay(5000); // wait 5 seconds
 
@@ -128,50 +134,76 @@ void MQTT_connect() {
 
 void loop() {
   io.run();
+//  wifiOn->save(0);
   MQTT_connect();
 
   // all code (processing) after this point
   // dummy variables to aggregate
-  int a = 0, b = 0;
-  float c = 0, d = 0;
+  int a = 0, b = 0, c = 0, d = 0;
+  int err = 0;
   float p10, p25;
-  int err;
+  float RS_gas = 0;
+  float ratio = 0;
+  float sensorValue = 0;
+  float sensor_volt = 0;
+  float R0 = 720;
   
   for (int i=0; i<1000; i++) {
     int MQ135reading = analogRead(MQ135);
     int MQ7reading = analogRead(MQ7);
-   
     a = a + MQ135reading;
     b = b + MQ7reading;
-    
   }
+
+  
+  int mqR = 22000; //pull-down resistor value
+  long rO = 41763; //rO sensor value
+  float minRsRo = 0.358; //min value for Rs/Ro
+  float maxRsRo = 2.428; //max value for Rs/Ro
+  float a7 = 116.6020682; //sensor a coefficient value
+  float b7 = -2.769034857; //sensor b coefficient value
+
+  // tidying up the values with exponential regression function
+  int averageMQ135 = a/1000;
+  int averageMQ7 = b/1000;
+
+  
+  int adcRaw = averageMQ7;
+  long rS = ((1024.0 * mqR) / adcRaw) - mqR;
+//  Serial.print("Rs: ");
+//  Serial.println(rS);
+  float rSrO = (float)rS / (float)rO;
+//  Serial.print("Rs/Ro: ");
+//  Serial.println(rSrO);
+
+  float ppm = a7 * pow((float)rS / (float)rO, b7);
+
+
   err = my_sds.read(&p25, &p10);
   c = p25;
   d = p10;
 
-  // output the results
-  int averageMQ135 = a/1000;
-  int averageMQ7 = b/1000;
   float averagePM25 = c;
   float averagePM10 = d;
-//  Serial.print("MQ135 reading (hazardous gases): ");
-//  Serial.println(averageMQ135);
-//  MQ135digital->save(averageMQ135);
-//  
-//  Serial.print("MQ7 reading (CO): ");
-//  Serial.println(averageMQ7);
-//  MQ7digital->save(averageMQ7);
+
+  // output the results
+  Serial.print("MQ135 reading (hazardous gases): ");
+  Serial.println(averageMQ135);
+  MQ135digital->save(averageMQ135);
+  
+  Serial.print("MQ7 reading (CO): ");
+  Serial.println(ppm);
+  MQ7digital->save(ppm);
 
   Serial.print("PM2.5 reading: ");
   Serial.println(String(averagePM25));
   PM25digital->save(averagePM25);
-
 
   Serial.print("PM10 reading: ");
   Serial.println(String(p10));
   PM10digital->save(averagePM10);
 
 
-  delay(5000);
+  delay(10000);
 }
   
